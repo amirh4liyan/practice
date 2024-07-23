f451f85ab2934d4ae5279da9a3315d9dfe5ef5bb
Amirhossein Alian <alianmail81@gmail.com>
2024-07-23 16:28:36 +0330

    First Code

---
+++ if_else.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    cout << "Enter N: ";
+    cin >> N;
+    
+    if (N % 2 == 0)
+        cout << "Dog";
+    else if (N % 5 == 0)
+        cout << "Cat";
+    else if (N % 2 == 0 and N % 5 == 0)
+        cout << "Dog";
+    else if (N % 2 == 0 and N % 5 == 0)
+        cout << "None";
+    return 0;
+}
